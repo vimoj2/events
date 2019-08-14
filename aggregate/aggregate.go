0ebe7ee76075e0e060a946a9da669186716dec1e
Eugene Zaidun <y.zaidun@zoover.nl>
2019-08-14 15:34:59 +0200

    Move code in subfolder

---
+++ aggregate/aggregate.go
@@ -0,0 +1,5 @@
+package aggregate
+
+func GetAgg() string {
+	return "GetAgg"
+}
--- main/main.go
+++ main/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/vimoj2/events/aggregate"
 	"github.com/vimoj2/golibs"
 )
 
 func main() {
 	fmt.Println("Hello world" + " " + golibs.GetRandName())
+	fmt.Print("Hello world" + " " + aggregate.GetAgg())
 }
